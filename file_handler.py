468a936d03e2869f37e2ffdc287e4c6f7b0a0308
g4ndalfTheCreator <zampee01@gmail.com>
2022-06-14 02:30:58 +0300

    File handler now returns false or true wheter an operation has been success

--- file_handler.py
+++ file_handler.py
@@ -1,9 +1,10 @@
+from unittest import result
 from encrypter import Encrypter
 
 class FileHandler:
         # Handles file writing and reading
 
-    def __init__(self, filename, key='samppa'):
+    def __init__(self, filename, key):
         # Initializes file writing
 
         self.__filename = filename
@@ -20,20 +21,26 @@ class FileHandler:
             for encrypted_row in file:
                 # Gets encrypted datarow from the file and decrypts it
 
-                current_row = self.__encrypter.decrypt(encrypted_row) 
-                current_row = current_row.strip()
-                current_row_in_a_list = current_row.split(";")
+                result = self.__encrypter.decrypt(encrypted_row)
+                result_of_decryption = result[0]
 
-                # Saves data into dict as {service: [username, password]}
-                self.__filedata[current_row_in_a_list[0]] = current_row_in_a_list[1:]
+                if result_of_decryption:
+                    # If the result is positive lets handle the file
+
+                    current_row = result[1]
+                    current_row = current_row.strip()
+                    current_row_in_a_list = current_row.split(";")
+
+                    # Saves data into dict as {service: [username, password]}
+                    self.__filedata[current_row_in_a_list[0]] = current_row_in_a_list[1:]
                 
+            file.close()
+            return result_of_decryption
+
         except OSError:
             # If opening the file fails return false/failed
 
-            print('Fail')
-
-        file.close()
-        
+            return False
   
     def write_file(self, database):
         # Tries to write in a file the current avialable database
@@ -54,21 +61,30 @@ class FileHandler:
                     line += ';' + current 
 
                 # Encrypts data and writes it into the file
-                encrypted_line = self.__encrypter.encrypt(line) 
-                print(encrypted_line, file=file_to_saved)
+                result = self.__encrypter.encrypt(line)
+                result_of_encryption = result[0]
+
+                if result_of_encryption:
+                    # If result of encryption is positive allows writing
+
+                    encrypted_line = result[1]
+                    print(encrypted_line, file=file_to_saved)
             
             file_to_saved.close()
+
+            return result_of_encryption
                 
         except OSError:
              # If writing in the file fails return false/failed
 
-            print('Fail')
+            return False
 
         
     def get_data(self):
         # Returns the data that was retrieved from the file after decryption
         return self.__filedata
 
+'''
 fileobj = FileHandler('testUser.txt')
 
 fileobj.open_file()
@@ -76,3 +92,4 @@ fileobj.open_file()
 print(fileobj.get_data())
 
 fileobj.write_file(fileobj.get_data())
+'''
